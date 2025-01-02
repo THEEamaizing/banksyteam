71feb5f168e951e62a5b154108bfeb476f75db6c
THEEamaizing <ambimwaluseke@gmail.com>
2025-01-02 18:36:56 +0300

    class initialization

--- bank.cpp
+++ bank.cpp
@@ -8,3 +8,11 @@ struct Account {
     string name;
     double balance;
 };
+class BankingSystem {
+private:
+    map<int, Account> accounts; // Store accounts using an account number as key
+    int nextAccountNumber;      // Generate unique account numbers
+
+public:
+    BankingSystem() : nextAccountNumber(1) {}
+}
