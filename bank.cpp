58aea75dcb4637370291203bb790e578d9cbe1fa
THEEamaizing <ambimwaluseke@gmail.com>
2025-01-02 18:34:54 +0300

    initial deployment

---
+++ bank.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <map>
+#include <string>
+using namespace std;
+
+// Structure to represent a Bank Account
+struct Account {
+    string name;
+    double balance;
+};
